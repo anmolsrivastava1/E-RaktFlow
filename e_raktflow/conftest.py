d3e0bb7d06625e9ac11ca32c19ce694bf17072f4
Anmol Srivastava <anmolsri.1@gmail.com>
2023-02-17 19:30:07 +0530

    initial commit

---
+++ e_raktflow/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from e_raktflow.users.models import User
+from e_raktflow.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
