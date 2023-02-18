399cc084c3992da2212495e6ef2d35f830a0e958
Anmol Srivastava <anmolsri.1@gmail.com>
2023-02-18 20:14:59 +0530

    updated allowed host

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="38f4pGtuEEUCoRaQBn6Lxxz3EjBxhVCV6pwvLnZJuZAYijAKQVvAjlau7pQi8jnM",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
