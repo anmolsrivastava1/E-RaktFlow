71c55de31c0fe5960cdec60b9e253457ed44566c
Anmol Srivastava <anmol.srivastava@createbytes.com>
2023-03-03 03:10:20 +0530

    integrated login api

--- RaktFlow/src/api/axios_requests.js
+++ RaktFlow/src/api/axios_requests.js
@@ -10,12 +10,35 @@ export const axiosPostRequest = async data => {
 };
 
 export const axiosOtpPostRequest = async (data, uuid) => {
-  const res = await axios({
-    method: 'patch',
-    url: `http://54.144.84.249:2001/users/verify-otp/${uuid}/`,
-    data: data,
-  });
-  console.log(uuid);
-  console.log('----');
-  return res;
+  try {
+    const res = await axios({
+      method: 'patch',
+      url: `http://54.144.84.249:2001/users/verify-otp/${uuid}/`,
+      data: data,
+    });
+    console.log(uuid);
+    console.log('----');
+    return res;
+  }
+  catch (err) {
+    console.log(Promise.reject(err.response.data));
+  }
+
+};
+
+export const LoginRequest = async ({ emailValue, passwordValue }) => {
+  try {
+    console.log(emailValue, passwordValue);
+    const res = await axios.post(`http://54.144.84.249:2001/users/login/`, {
+      "email": emailValue,
+      "password": passwordValue
+    });
+
+    console.log(res.data, "aaaaa");
+    return Promise.resolve(res);
+  } catch (err) {
+    console.log(err, "bbbbb");
+
+    return Promise.reject(err.response?.data);
+  }
 };
